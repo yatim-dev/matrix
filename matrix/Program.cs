4532cbb6c76923cf61d609c03c1f02c848281273
Eric56456 <goodlook19900@gmail.com>
2023-03-26 21:56:20 +0300

    I have correct mistakes

--- matrix/Program.cs
+++ matrix/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace matrix
 {
@@ -6,100 +7,77 @@ namespace matrix
     {
         public static int[] WriteNums(int cols)
         {
-            int[] result = new int[cols];
+            var result = new int[cols];
             var stringNums = Console.ReadLine().Split(' ');
-            for (int i = 0; i < cols; i++)
-            {
+            for (var i = 0; i < cols; i++) 
                 result[i] = int.Parse(stringNums[i]);
-
-            }
             return result;
         }
+
         public static int[,] CreateMatrix(int rows, int cols)
         {
             var matrix = new int[rows, cols];
 
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            for (var i = 0; i < matrix.GetLength(0); i++)
             {
-                int[] nums = WriteNums(cols);
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
+                var nums = WriteNums(cols);
+                for (var j = 0; j < matrix.GetLength(1); j++)
                     matrix[i, j] = nums[j];
-                }
             }
             return matrix;
         }
-        public static int[,] CreateRandomMatrix(int rows, int cols)
+
+        public static int[,] CreateRandomMatrix(int rows, int cols, int minArg = 0, int maxArg = 100)
         {
             var random = new Random();
             var matrix = new int[rows, cols];
 
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    matrix[i, j] = random.Next(0, 100);
-                }
-            }
+            for (var i = 0; i < matrix.GetLength(0); i++)
+                for (var j = 0; j < matrix.GetLength(1); j++)
+                    matrix[i, j] = random.Next(minArg, maxArg);
             return matrix;
         }
 
         public static int[,] CreateUnitMatrix(int rows)
         {
-            var random = new Random();
             var matrix = new int[rows, rows];
 
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    if (i == j)
-                    {
-                        matrix[i, j] = 1;
-                    }
-                    else
-                    {
-                        matrix[i, j] = 0;
-                    }
-                }
-            }
+            for (var i = 0; i < matrix.GetLength(0); i++)
+                for (var j = 0; j < matrix.GetLength(1); j++)
+                    _ = i == j ? matrix[i, j] = 1 : matrix[i, j] = 0;
             return matrix;
         }
 
         public static (int rows, int cols) ReadMatrixParams()
         {
             var words = Console.ReadLine().Split(' ');
-
             return (int.Parse(words[0]), int.Parse(words[1]));
         }
+
         private static void WriteMatrix(int[,] matrix)
         {
-            for (int i = 0; i < matrix.GetLength(0); i++)
-
+            for (var i = 0; i < matrix.GetLength(0); i++)
             {
-
-                Console.Write("|");
-                Console.Write(" ");
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    Console.Write(matrix[i, j]);
-                    Console.Write(" ");
-                }
-                Console.Write("|");
-                Console.WriteLine();
+                Console.WriteLine(BildingString(matrix, i));
             }
         }
-
-
+        private static string BildingString(int[,] matrix, int i)
+        {
+            var stringBuilder = new StringBuilder("| ");
+            for (var j = 0; j < matrix.GetLength(1); j++)
+            {
+                stringBuilder.Append(matrix[i, j]);
+                stringBuilder.Append(" ");
+            }
+            stringBuilder.Append("|");
+            return stringBuilder.ToString();
+        }
         public static void Main()
         {
             //           var matrixParams = ReadMatrixParams();
-            var matrix = CreateUnitMatrix(3);
+            var matrix = CreateRandomMatrix(3,3);
             WriteMatrix(matrix);
             Console.ReadLine();
-
         }
-
-
     }
 }
